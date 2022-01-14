18d8d8e7e70dea6678d87f85863700fd5350bdff
Stefanowhb <3473272+Stefanowhb@users.noreply.github.com>
2022-01-14 18:33:07 +0100

    Add default ByteBufferRead implementations to byte_buffer crate

--- bytey_byte_buffer/src/byte_buffer_read.rs
+++ bytey_byte_buffer/src/byte_buffer_read.rs
@@ -6,3 +6,25 @@ pub trait ByteBufferRead {
 	fn read_from_buffer_le(buffer: &mut ByteBuffer) -> Result<Self> where Self: Sized;
 	fn read_from_buffer_be(buffer: &mut ByteBuffer) -> Result<Self> where Self: Sized;
 }
+
+macro_rules! impl_byte_buffer_read_types {
+	($($type:ty),*) => {
+		$(
+			impl ByteBufferRead for $type {
+				fn read_from_buffer(buffer: &mut ByteBuffer) -> Result<$type> {
+					Ok(<$type>::from_ne_bytes(buffer.read_slice(std::mem::size_of::<$type>())?.try_into().expect("This really shouldn't fail")))
+				}
+
+				fn read_from_buffer_le(buffer: &mut ByteBuffer) -> Result<$type> {
+					Ok(<$type>::from_le_bytes(buffer.read_slice(std::mem::size_of::<$type>())?.try_into().expect("This really shouldn't fail")))
+				}
+
+				fn read_from_buffer_be(buffer: &mut ByteBuffer) -> Result<$type> {
+					Ok(<$type>::from_be_bytes(buffer.read_slice(std::mem::size_of::<$type>())?.try_into().expect("This really shouldn't fail")))
+				}
+			}
+		)*
+	}
+}
+
+impl_byte_buffer_read_types!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, f32, f64);
